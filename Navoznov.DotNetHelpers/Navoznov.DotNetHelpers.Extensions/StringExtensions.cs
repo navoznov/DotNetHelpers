6ed8b561ec15b6859a1d307d0c314897a7c2704a
navoznov <ivan.navoznov@gmail.com>
2016-04-29 17:29:19 +0300

    метод-расширение F

---
+++ Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Navoznov.DotNetHelpers.Extensions
+{
+    /// <summary>
+    /// Методы расширения для класса <see cref="string"/>
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Более удобная обертка над string.Format(...)
+        /// </summary>
+        /// <param name="format">строка-шаблон</param>
+        /// <param name="args">аргументы, подставляемые в шаблон</param>
+        /// <returns></returns>
+        public static string F(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
+}
