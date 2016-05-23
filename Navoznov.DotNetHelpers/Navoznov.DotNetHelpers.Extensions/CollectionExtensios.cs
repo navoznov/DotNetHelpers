25e17d2214638e6985eef020d51f5ac8c63e4e3b
navoznov <ivan.navoznov@gmail.com>
2016-05-23 17:29:41 +0300

    метод расширений ForEach для ICollection

---
+++ Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/CollectionExtensios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Navoznov.DotNetHelpers.Extensions
+{
+    /// <summary>
+    /// Методы-расширения для <see cref="ICollection{T}"/>
+    /// </summary>
+    public static class CollectionExtensios
+    {
+        /// <summary>
+        /// Выполняет метод <paramref name="action"/> последовательно для каждого элемента в коллекции <paramref name="collection"/>
+        /// </summary>
+        /// <param name="collection">коллекция элементов</param>
+        /// <param name="action">метод, который надо выполнить для каждого из элементов коллекции <paramref name="collection"/></param>
+        public static void ForEach<T>(this ICollection<T> collection, Action<T> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
--- Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/StringExtensions.cs
+++ Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/StringExtensions.cs
@@ -10,10 +10,19 @@
         /// </summary>
         /// <param name="format">строка-шаблон</param>
         /// <param name="args">аргументы, подставляемые в шаблон</param>
-        /// <returns></returns>
-        public static string F(this string format, params object[] args)
+        public static string Format(this string format, params object[] args)
         {
             return string.Format(format, args);
+        }
+
+        /// <summary>
+        /// Более удобная обертка над string.Format(...)
+        /// </summary>
+        /// <param name="format">строка-шаблон</param>
+        /// <param name="args">аргументы, подставляемые в шаблон</param>
+        public static string F(this string format, params object[] args)
+        {
+            return Format(format,args);
         }
     }
 }
