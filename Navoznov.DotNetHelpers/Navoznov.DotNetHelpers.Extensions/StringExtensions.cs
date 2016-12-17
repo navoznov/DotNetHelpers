c4b7ea23d0db2b33f4342129f34007e037df90bb
imbeat <ivan.navoznov@gmail.com>
2016-12-17 09:49:55 +0300

    Метод расширение Slice - аналог slice в JS

--- Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/StringExtensions.cs
+++ Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/StringExtensions.cs
@@ -24,5 +24,25 @@
         {
             return Format(format,args);
         }
+        
+        /// <summary>
+        /// Аналог функции slice из языка JavaScript.
+        /// </summary>
+        /// <param name="str">строка</param>
+        /// <param name="startIndex">индекс начала подстроки. Если меньше нуля, то отсчитывается с конца строки.</param>
+        /// <param name="endIndex">индекс конца подстроки (невключается в результат). Если меньше нуля, то отсчитывается с конца строки.</param>
+        /// <returns>подстрока</returns>
+        /// <example>
+        /// <code>
+        /// "Hello world!".Slice(-1);   // "!"
+        /// "Hello world!".Slice(1,-3);   // "ello wor"
+        /// </code>
+        /// </example>
+        public static string Slice(this string str, int startIndex, int endIndex = 0)
+        {
+            var realStartIndex = startIndex < 0 ? str.Length + startIndex : startIndex;
+            var realEndIndex = endIndex <= 0 ? str.Length + endIndex : endIndex;
+            return str.Substring(realStartIndex, realEndIndex - realStartIndex);
+        }
     }
 }
