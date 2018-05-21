88b7762b4970bc51e266604459c96eab0d92fdf2
navoznov <ivan.navoznov@gmail.com>
2018-05-21 12:21:10 +0300

    добавлены методы расширения для StringBuilder

---
+++ Navoznov.DotNetHelpers/Navoznov.DotNetHelpers.Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Navoznov.DotNetHelpers.Extensions
+{
+    /// <summary>
+    /// Методы расширения для <see cref="StringBuilder"/>
+    /// </summary>
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Добавляет строку если выполняется условие
+        /// </summary>
+        /// <param name="sb">инстанс <see cref="StringBuilder"/></param>
+        /// <param name="condition">условие</param>
+        /// <param name="str">строка</param>
+        /// <returns>тот же самый инстанс <see cref="StringBuilder"/></returns>
+        public static StringBuilder AppendIf(this StringBuilder sb, bool condition, string str)
+        {
+            if (sb == null)
+            {
+                throw new ArgumentNullException(nameof(sb));
+            }
+
+            return condition ? sb.Append(str) : sb;
+        }
+
+        /// <summary>
+        /// Добавляет строку и перенос на новую строку, если выполняется условие
+        /// </summary>
+        /// <param name="sb">инстанс <see cref="StringBuilder"/></param>
+        /// <param name="condition">условие</param>
+        /// <param name="str">строка</param>
+        /// <returns>тот же самый инстанс <see cref="StringBuilder"/></returns>
+        public static StringBuilder AppendLineIf(this StringBuilder sb, bool condition, string str)
+        {
+            if (sb == null)
+            {
+                throw new ArgumentNullException(nameof(sb));
+            }
+
+            return condition ? sb.Append(str).AppendLine() : sb;
+        }
+    }
+}
